2d7d320a1a4934e50161595847e87c292b5a0d65
mycodeinpeace <me@codeinpeace.com>
2022-07-29 11:29:50 +0300

    Step 2 – Permissions: Allow only the Owner to Withdraw Ether

--- EthereumBlockchainDeveloperBootcampWithSolidity/SharedWalletwithAllowanceFunction/SharedWallet.sol
+++ EthereumBlockchainDeveloperBootcampWithSolidity/SharedWalletwithAllowanceFunction/SharedWallet.sol
@@ -3,7 +3,18 @@ pragma solidity ^0.6.0;
 
 contract SharedWallet {
 
-    function withdrawMoney(address payable _to, uint _amount) public {
+    address public owner;
+
+    constructor() internal {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not allowed");
+        _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
     
