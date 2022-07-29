a7dd3852f93114703e40570e90da835064499901
mycodeinpeace <me@codeinpeace.com>
2022-07-29 12:51:34 +0300

    Step 8 – Add Events in the SharedWallet Smart Contract

--- EthereumBlockchainDeveloperBootcampWithSolidity/SharedWalletwithAllowanceFunction/SharedWallet.sol
+++ EthereumBlockchainDeveloperBootcampWithSolidity/SharedWalletwithAllowanceFunction/SharedWallet.sol
@@ -27,15 +27,20 @@ contract Allowance is Ownable {
 
 contract SharedWallet is Ownable, Allowance {
 
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
 
         require(_amount <= address(this).balance, "Contract doesn't own enough money");
         if (msg.sender != owner()) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
     receive () external payable {
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
