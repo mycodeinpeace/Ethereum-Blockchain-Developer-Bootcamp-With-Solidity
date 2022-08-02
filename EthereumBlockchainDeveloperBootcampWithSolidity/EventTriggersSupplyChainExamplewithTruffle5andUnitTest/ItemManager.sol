eba4daea34eb6dfc3f937d0d21343119c410b7ed
mycodeinpeace <me@codeinpeace.com>
2022-08-02 22:40:27 +0300

    Solidity Part Improve Approach 1 for flawless UX for Endusers

--- EthereumBlockchainDeveloperBootcampWithSolidity/EventTriggersSupplyChainExamplewithTruffle5andUnitTest/ItemManager.sol
+++ EthereumBlockchainDeveloperBootcampWithSolidity/EventTriggersSupplyChainExamplewithTruffle5andUnitTest/ItemManager.sol
@@ -1,11 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
+
+contract Item {
+    uint public priceInWei;
+    uint public pricePaid;
+    uint public index;
+
+    ItemManager parentContract;
+
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
+        priceInWei = _priceInWei;
+        index = _index;
+        parentContract = _parentContract;
+    }
+
+    receive() external payable {
+        require(msg.value == priceInWei, "We don't support partial payments");
+        require(priceInWei == 0, "Item is already paid!");
+        pricePaid += msg.value;
+        (bool success, ) = address(parentContract).call.value(msg.value)(abi.encodeWithSignature("triggerPayment(uint256)", index));
+        require(success, "Transaction was not successfull, cancelling");
+    }
+
+    fallback () external {
+
+    }
+}
 
 contract ItemManager {
 
     enum SupplyChainState {Created, Paid, Delivered}
 
     struct S_Item {
+        Item _item;
         string _identifier;
         uint _itemPrice;
         ItemManager.SupplyChainState _state;
@@ -14,14 +41,16 @@ contract ItemManager {
     mapping(uint => S_Item) public items;
     uint itemIndex;
 
-    event SupplyChainStep(uint _itemIndex, uint _step);
+    event SupplyChainStep(uint _itemIndex, uint _step, address _itemAddress);
 
     function createItem (string memory _identifier, uint _itemPrice) public {
+        Item item = new Item(this, _itemPrice, itemIndex);
+        items[itemIndex]._item = item;
         items[itemIndex]._identifier = _identifier;
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
 
-        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state));
+        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state), address(item));
         itemIndex++;
     }
 
@@ -31,7 +60,7 @@ contract ItemManager {
 
         items[_itemIndex]._state = SupplyChainState.Paid;
 
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state));
+        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
     }
 
     function tiggerDelivery(uint _itemIndex) public {
@@ -39,6 +68,6 @@ contract ItemManager {
 
         items[_itemIndex]._state = SupplyChainState.Delivered;
 
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state));
+        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
     }
 }
