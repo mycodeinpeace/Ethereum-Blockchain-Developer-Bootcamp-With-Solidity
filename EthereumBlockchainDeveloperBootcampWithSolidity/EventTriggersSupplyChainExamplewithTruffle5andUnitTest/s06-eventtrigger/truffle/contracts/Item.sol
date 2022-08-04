4e85ea7d74b20cfbda0a96947121b8d149ae51e1
mycodeinpeace <me@codeinpeace.com>
2022-08-04 22:14:07 +0300

    Replace Smart Contracts in the Truffle React Box

---
+++ EthereumBlockchainDeveloperBootcampWithSolidity/EventTriggersSupplyChainExamplewithTruffle5andUnitTest/s06-eventtrigger/truffle/contracts/Item.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+import "./ItemManager.sol";
+
+contract Item {
+    uint public priceInWei;
+    uint public pricePaid;
+    uint public index;
+
+    ItemManager parentContract;
+
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
+        priceInWei = _priceInWei;
+        index = _index;
+        parentContract = _parentContract;
+    }
+
+    receive() external payable {
+        require(msg.value == priceInWei, "We don't support partial payments");
+        require(priceInWei == 0, "Item is already paid!");
+        pricePaid += msg.value;
+        (bool success, ) = address(parentContract).call.value(msg.value)(abi.encodeWithSignature("triggerPayment(uint256)", index));
+        require(success, "Transaction was not successfull, cancelling");
+    }
+
+    fallback () external {
+
+    }
+}
