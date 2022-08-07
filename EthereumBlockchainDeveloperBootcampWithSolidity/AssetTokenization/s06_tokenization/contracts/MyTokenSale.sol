7e1d85033ab7e8df9f336b813a106228091de1a4
mycodeinpeace <me@codeinpeace.com>
2022-08-07 22:44:37 +0300

    Add The Migration For The MyTokenSale Smart Contract
    truffle migrate

--- EthereumBlockchainDeveloperBootcampWithSolidity/AssetTokenization/s06_tokenization/contracts/MyTokenSale.sol
+++ EthereumBlockchainDeveloperBootcampWithSolidity/AssetTokenization/s06_tokenization/contracts/MyTokenSale.sol
@@ -4,7 +4,6 @@ import "./Crowdsale.sol";
 
 contract MyTokenSale is Crowdsale {
 
-    KycContract kyc;
     constructor(
         uint256 rate,    // rate in TKNbits
         address payable wallet,
