0521c91ba06da4fc8882be5a15e24ddcae126573
mycodeinpeace <me@codeinpeace.com>
2022-08-08 12:45:37 +0300

    Consolidate Configuration in Truffle into One .env File using dotenv

--- EthereumBlockchainDeveloperBootcampWithSolidity/AssetTokenization/s06_tokenization/test/MyToken.test.js
+++ EthereumBlockchainDeveloperBootcampWithSolidity/AssetTokenization/s06_tokenization/test/MyToken.test.js
@@ -11,11 +11,13 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
+require('dotenv').config({path: '../.env'});
+
 contract("Token Test", function(accounts) {
     const [ initialHolder, recipient, anotherAccount ] = accounts;
 
     beforeEach(async () => {
-        this.myToken = await Token.new(1000);
+        this.myToken = await Token.new(process.env.INITIAL_TOKENS);
     });
 
     it("All tokens should be in my account", async () => {
