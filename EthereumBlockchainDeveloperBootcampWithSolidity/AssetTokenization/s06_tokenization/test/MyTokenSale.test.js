ca89ae4dfae9c4bf271ae4135cbc02853cdea783
mycodeinpeace <me@codeinpeace.com>
2022-08-08 13:10:10 +0300

    Fix Multiple Truffle Tests Using A Common Setup For Chai and Chai Expect

--- EthereumBlockchainDeveloperBootcampWithSolidity/AssetTokenization/s06_tokenization/test/MyTokenSale.test.js
+++ EthereumBlockchainDeveloperBootcampWithSolidity/AssetTokenization/s06_tokenization/test/MyTokenSale.test.js
@@ -1,21 +1,16 @@
 const Token = artifacts.require("MyToken");
 const TokenSale = artifacts.require("MyTokenSale");
 
-var chai = require("chai");
-const expect = chai.expect;
-
+const chai = require("./chaisetup.js");
 const BN = web3.utils.BN;
-const chaiBN = require('chai-bn')(BN);
-chai.use(chaiBN);
-
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
+const expect = chai.expect;
 
 contract("TokenSale", async function(accounts) {
     const [ initialHolder, recipient, anotherAccount ] = accounts;
 
-it("there shouldnt be any coins in my account", async () => {
-    let instance = await Token.deployed();
-    expect(instance.balanceOf.call(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(0));
+    it("there shouldnt be any coins in my account", async () => {
+        let instance = await Token.deployed();
+        return expect(instance.balanceOf.call(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(0));
     });
+
 });
